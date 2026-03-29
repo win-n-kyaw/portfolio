ce47964fcce9a42b765bf663e010ee6df09b8f21
win-n-kyaw <winnkyawhc@hotmail.com>
2026-03-29 17:41:13 +0700

    Resume on Sanity

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
   name: 'portfolio',
   title: 'Portfolio CMS',
 
-  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
-  dataset: import.meta.env.PUBLIC_SANITY_DATASET ?? 'production',
+  projectId: import.meta.env.SANITY_STUDIO_PROJECT_ID || import.meta.env.PUBLIC_SANITY_PROJECT_ID,
+  dataset: import.meta.env.SANITY_STUDIO_DATASET || import.meta.env.PUBLIC_SANITY_DATASET || 'production',
 
   plugins: [structureTool(), visionTool(), codeInput(),],
 
