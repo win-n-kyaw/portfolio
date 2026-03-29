f4b38f457450555ea22f6c47f4dd03db70183e2d
win-n-kyaw <winnkyawhc@hotmail.com>
2026-03-29 11:49:50 +0700

    added sanity cofig

---
+++ sanity.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'sanity';
+import { structureTool } from 'sanity/structure';
+import { codeInput } from '@sanity/code-input';
+import { visionTool } from '@sanity/vision';
+import { schemaTypes } from './sanity/schemas';
+
+export default defineConfig({
+  name: 'portfolio',
+  title: 'Portfolio CMS',
+
+  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
+  dataset: import.meta.env.PUBLIC_SANITY_DATASET ?? 'production',
+
+  plugins: [structureTool(), visionTool(), codeInput()],
+
+  schema: {
+    types: schemaTypes,
+  },
+});
